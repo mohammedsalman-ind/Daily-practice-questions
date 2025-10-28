a4ff62f1a8a88939c5158c177260200a2bddb719
mohammedsalman-ind <mohammedsalman2400@gmail.com>
2025-10-28 23:29:06 +0530

    Add files via upload

---
+++ 04_daily_question.c
@@ -0,0 +1,10 @@
+// Print even numbers between 1 and 50.
+#include <stdio.h>
+int main (){
+    for(int i=1;i<=50;i++){
+        if(i%2==0){
+            printf("%d",i);
+            printf("\n");
+        }
+    }
+}
