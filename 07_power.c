923c34ba65927ced7043b26c50bfd9e11114f5c8
mohammedsalman-ind <mohammedsalman2400@gmail.com>
2025-11-01 23:46:57 +0530

    Add power calculation program in 07_power.c
    Implement a program to calculate the power of a number.

---
+++ 07_power.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main() {
+    double num ;
+    double paw;
+    double output;
+    printf("Enter number:");
+    scanf("%lf",&num);
+    printf("\n");
+    printf("Enter power:");
+    scanf("%lf",&paw);
+    for(int i=1;i<=paw;i++){
+        output=num*paw;
+        
+    }
+    printf("%lf",num);
+    return 0;
+}
+    double calculation;
