743793059dabe6b84de775d797141dccfef61a5f
mohammedsalman-ind <mohammedsalman2400@gmail.com>
2025-11-17 00:07:09 +0530

    Fix Fibonacci series output for num 0 and 1

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -2,19 +2,19 @@
 int main (){
     int num;
     scanf("%d",&num);
-    if(num==1){
-        printf("1");
+    if(num==1 || num==0){
+        printf("0");
     }
     else{
         int a=0,b=1;
-        printf("%d ,",a);
+        printf("%d , ", a);
         printf("%d",b);
-        for(int i=0;i<=num;i++){
+        for(int i=1;i<=num-2;i++){
             int c=a+b;
+            printf(" , %d  ",c);
             a=b;
             b=c;
 
-            printf(", %d  ",c);
 
         }
         
